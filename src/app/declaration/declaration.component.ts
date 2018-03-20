997604c9ae08ee2a6127679e9c98d42e5e0e660a
Kienan Bachwa <kienan.bachwa@etu.unice.fr>
2018-03-20 11:56:33 +0100

    post fonctionnel (en utilisant du javascript, pas angular)

--- src/app/declaration/declaration.component.ts
+++ src/app/declaration/declaration.component.ts
@@ -1,12 +1,46 @@
 import {Component, OnInit} from "@angular/core";
+import {IncidentService} from "../shared/services/incident.service";
+import {IncidentModel} from "../shared/models/Incident";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
 
 @Component({
     selector: "app-declaration",
     templateUrl: "./declaration.component.html",
     styleUrls: ["./declaration.component.css"]
 })
+
+
 export class DeclarationComponent implements OnInit {
 
-    ngOnInit() {
+    constructor(private incidentService: IncidentService, private http: HttpClient) {
+        this.incidentService = incidentService;
     }
+
+    ngOnInit() {
+        console.log("test");
+    }
+
+
+    addIncident() {
+        var url = "http://localhost:3000/declaration";
+        var method = "POST";
+        var postData = '{"title":"UnTitle"}';
+        var shouldBeAsync = true;
+        var request = new XMLHttpRequest();
+
+        request.onload = function () {
+            var status = request.status;
+            var data = request.responseText;
+        };
+
+        request.open(method, url, shouldBeAsync);
+
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+        request.send(postData);
+
+    }
+
+
 }
--- src/app/shared/services/incident.service.ts
+++ src/app/shared/services/incident.service.ts
@@ -1,23 +1,46 @@
 import {Injectable} from "@angular/core";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import {IncidentModel} from "../models/Incident";
 import {URL_SERVER} from "../constants/urls";
+import {Observable} from "rxjs/Observable";
+import {tap, catchError} from "rxjs/operators";
 
 @Injectable()
 export class IncidentService {
 
     public incidentList: BehaviorSubject<IncidentModel[]>;
 
-    private route = "/incidents";
+    private routeVue = "/incidents";
+    private routeDeclaration = "/declaration";
+
+    httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
 
     constructor(private http: HttpClient) {
         this.incidentList = new BehaviorSubject([]);
     }
 
     getIncident() {
-        this.http.get<IncidentModel[]>(URL_SERVER + this.route).subscribe((incidents) => this.incidentList.next(incidents));
+        this.http.get<IncidentModel[]>(URL_SERVER + this.routeVue).subscribe((incidents) => this.incidentList.next(incidents));
     }
+
+    /*
+    addIncident(){
+        console.log("AJOUT INCIDENT");
+        var url = 'http://localhost:3000/declaration';
+        var data = {title:"titre", description:"description"};
+        this.http.post(url, data);
+    }
+
+    addIncident (incident: IncidentModel): Observable<IncidentModel> {
+        return this.http.post<IncidentModel>(this.routeDeclaration, incident, this.httpOptions).pipe(
+            tap((incident: IncidentModel) => this.log(`added incident w/ id=${incident.id}`)),
+            catchError(this.handleError<IncidentModel>('addIncident'))
+        );
+    }
+    */
 
 }
