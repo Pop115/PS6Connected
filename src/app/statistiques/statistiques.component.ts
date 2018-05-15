b84657b49bec6c6f6e0c29dc70ecce0019ff09f7
Kienan Bachwa <kienan.bachwa@etu.unice.fr>
2018-05-15 13:34:42 +0200

    Page statistiques (nécessite le lancement de elasticsearch et kibana)

---
+++ src/app/statistiques/statistiques.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: "statistiques",
+    templateUrl: "./statistiques.component.html",
+    styleUrls: ["./statistiques.component.css"]
+})
+
+export class StatistiquesComponent implements OnInit {
+
+    ngOnInit() {
+    }
+
+
+
+}
